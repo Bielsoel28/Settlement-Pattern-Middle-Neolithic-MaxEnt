aafc2b8bdf54ffd1937a9820896a75c808f28400
Biel Soriano Elias <bielsoel28@gmail.com>
2026-01-13 18:24:59 +0100

    Update Variables creation.R

--- Code/Variables creation.R
+++ Code/Variables creation.R
@@ -24,7 +24,7 @@ rm(list=ls())
 
 # Needed packages
 packages <- c("leastcostpath","terra", "sf", "dplyr","tidyr", "doParallel", "viewscape",
-              "ggplot2","spatstat","GA","stars","corrplot","maxnet","blockCV")
+              "ggplot2","spatstat","GA","stars","corrplot","maxnet","blockCV","cli")
 
 #Optional, Run this if the pacakges are not already installed
 for (packages in packages) {
